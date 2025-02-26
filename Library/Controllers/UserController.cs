532346afd503ee91626835b07b852ad2221e31bf
Waldo Heudens <waldo@heudens.be>
2025-02-26 23:34:02 +0100

    Start van User-beheer

---
+++ Library/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Library.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+
+    [Authorize (Roles = "UserAdmin")]
+    public class UsersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public UsersController (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Users.Where(u => u.UserName != "?").OrderBy(u => u.UserName));
+        }
+
+        public IActionResult Roles()
+        {
+
+            return View();
+        }
+    }
+}
